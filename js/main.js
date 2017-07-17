e097fcf88f3d44a0be7af46daaeafc62858198fe
Youss06 <oumar.youssouf0605@gmail.com>
2017-07-17 18:42:50 +0200

    Change js link to img

--- js/main.js
+++ js/main.js
@@ -1,34 +1,121 @@
-var  choix1= "";
-var choix2= "";
-var images= ["img/52.jpg","img/125.jpg","img/bart.jpg","img/burns.jpg","img/homer.jpg","img/lisa-simpson-profile.jpg","img/margepost1.jpg"]
+var tab = ["img/52.jpg","img/125.jpg","img/bart.jpg","img/burns.jpg","img/homer.jpg","img/lisa-simpson-profile.jpg","img/margepost1.jpg"];
+var precedImg = ""; var precedAttr = "";										//Variables de "sauvegardes"
+var click = 0;											//Varible gerant les click
+// var great = 0; var lose = 0;							// Variable victoire et défaite
+// var chron;												//Contiendra le chronometre
+// var temps = 1; var secondes = 60;
+// var chrono = document.getElementById("chrono");
+var partieEnCours = true;								//Gere le lancement du chronometre
+var ready = true;										//Gere l'accès au clicks
 
-function choix(x) {
-document.getElementsByName('img').innerHTML = choix1;
-document.getElementsByName('img').innerHTML = choix2;
-game();
+function shuffle(a) {										//Fonction shuffle: melange le tableau tab
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+shuffle(tab);
+
+function retourne(id, name){						//FONCTION RETOURNE
+	// if(partieEnCours){									//Au premier click, lancement du chrono
+		// chron = setInterval("chronometre()", 1000);		//Appelle la fonction chronometre toute les secondes
+		// partieEnCours = false							//Rend la condition fausse pour éviter les multiple lancement de setInterval
+	// }
+	if(ready){											//Si ready = true, alors on peu cliquer
+		name = document.getElementById(name);			//Recupération de l'image qui appelle la fonction
+		for(i = 1; i <= tab.length; i++){
+			switch(id){
+				case i:
+					name.src =tab[i-1];					//Attribution des sources du tableau à chaque images cliquées
+					var attr = "retourne("+(i)+", '"+(i)+"')";
+					name.removeAttribute("onclick");
+					if(click == 0){						//Au premier click et seulement à celui-ci
+						precedImg = name;				//Attribution d'une valeur "sauvegarde" pour comparaison
+						precedAttr = attr;
+						click = 1;						//Empeche de réécrire la sauvegarde au second click
+					}
+					else{
+						click = 2;
+					}
+				break;
+			}
+		}
+	 	if(name.src != precedImg.src && click == 2){
+	 		ready = false;								//On empeche les click durant la phase de comparaison
+			setTimeout(function(){						//Permet de lancer la fonction avec un retard choisit(ici 2secondes)
+				name.src = "img/jeu_de_carte_katag_mystere-220x300.png";			//Retourne les cartes pour les mettre à l'envers
+				name.setAttribute("onclick", attr);
+				precedImg.src = "img/jeu_de_carte_katag_mystere-220x300.png";
+				precedImg.setAttribute("onclick", precedAttr);
+				precedImg = "";							//Effacement de la "sauvegarde" pour la prochaine phase de click
+				click = 0;
+				ready = true;							//Réautorisation des clicks
+			}, 2000);
+		}
+		else if(name.src == precedImg.src && click == 2){		//Si 2 images sont identiques
+			click = 0;
+			// great++;											//Ajoute un point
+		}
+	}
+	// victoire();
 }
 
-function game(choix){
-var choix1 = Math.floor((Math.random() * 7) + 1);
+// function chronometre(){
+// 	if(temps >= 0){
+// 		secondes--;
+// 		if(secondes >= 10)
+// 			document.getElementById("chrono").innerHTML = temps+":"+secondes;
+// 		else if(secondes < 10){
+// 			document.getElementById("chrono").innerHTML = temps+":0"+secondes;
+// 			if(secondes == 0){
+// 				secondes = 60;
+// 				temps--;
+// 			}
+// 		}
+// 	}
+// 	else{
+// 			lose = 1;
+// 			victoire();
+// 			clearInterval(chron);
+// 		}
+// }
+//
+// function victoire(){
+// 	if(great == 6){
+// 		clearInterval(chron);
+// 		recommencer("gagné");
+//
+// 	}
+// 	else if(lose == 1){
+// 		ready = false;
+// 		clearInterval(chron);
+// 		recommencer("perdu");
+// 	}
+// }
 
-if(choix1==1){
-  choix1="img/52.jpg";
-} else if (choix1==2){
-   choix1="img/125.jpg";
-} else if (choix1==3){
-   choix1="img/bart.jpg";
-}
-else if (choix1=4){
-   choix1="img/burns.jpg";
-}
-else if (choix1==5){
-   choix1="img/homer.jpg";
-}
-else if (choix1==6){
-   choix1="img/lisa-simpson-profile.jpg";
-}
-else {
- choix1="img/margepost1.jpg";
-}
-document.getElementsByTagName('img').innerHTML = choix1;
-}
+// function recommencer(victoire){
+// 	clearInterval(chron);
+// 	var retry = true;
+// 	if(victoire)
+// 		retry = confirm("Vous avez "+victoire+" !\n Recommencer ?");
+// 	if(retry){
+// 		var ZJeu = document.getElementsByClassName("carte");
+// 		for(i = 0; i < ZJeu.length; i++){
+// 			ZJeu[i].src = "img/jeu_de_carte_katag_mystere-220x300.png";
+// 			ZJeu[i].setAttribute("onclick", "retourne("+(i+1)+", '"+(i+1)+"')");
+// 		}
+// 		great = 0; lose = 0;
+// 		ready = true;
+// 		partieEnCours = true;
+// 		temps = 1; secondes = 60;
+// 		document.getElementById("chrono").innerHTML = "2:00";
+// 		shuffle(tab);
+// 	}
+// 	else{
+// 		ready = false;
+// 		great = 0; lose = 0;
+// 	}
+// }
